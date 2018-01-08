f1e1b25a3559c23f310a8a0c61f65845fd74a696
elaine <elaine@nano.openair4G.eur>
2018-01-08 14:32:26 +0800

    To listen the POST and insert into database

---
+++ app.js
@@ -0,0 +1,34 @@
+var coap = require('coap');
+var mysql = require('mysql');
+
+var server = coap.createServer();
+var config = require('../coap/db-config.json');
+var pool = mysql.createPool(config)
+
+server.on('request', function(req,res){
+	console.log('get the data');
+	var payload = req.payload.toString();
+	var ser = new Array();
+	ser = payload.split(',');
+	var temp = ser[0];
+	var hum = ser[1];
+	var light = ser[2];
+	var device_id = ser[3];
+	console.log('device id:%s, temperature:%f, humidity:%f, light:%f \n',device_id,temp,hum,light);
+
+	pool.getConnection(function(err, connection){
+		connection.query('insert into sensor_history set device_id=?, temp=?, hum = ?,light=?,time=NOW()',
+			[device_id,temp,hum,light],
+			function(err, results){
+				if(err){
+					console.log(err.message);
+				}
+				console.log('Data insert successfully!');
+				connection.release;
+			})
+	})
+})
+
+server.listen(function() {
+  console.log('server started')
+}) 
